78b2408323d95dd9b6fb2699b52cadcf2432180e
Greg Sieranski <git@wonbyte.com>
2024-12-26 23:19:23 -0500

    nvim: completion

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,7 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    dependencies = "saghen/blink.cmp",
     opts = {
       -- Diagnostics Configuration
       diagnostics = {
@@ -75,7 +76,7 @@ return {
 
       -- Set up each server
       for server, server_opts in pairs(opts.servers) do
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        local capabilities = require("blink.cmp").get_lsp_capabilities()
 
         lspconfig[server].setup(vim.tbl_deep_extend("force", {
           capabilities = capabilities,
