eb24c5dd5490f96eba9fd1605c2b2fc1fe84b07f
Greg Sieranski <git@wonbyte.com>
2024-12-27 00:03:28 -0500

    nvim: improve telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,79 +6,86 @@ return {
       { "nvim-lua/plenary.nvim" },
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
-    opts = {
-      defaults = {
-        layout_config = {
-          horizontal = {
-            -- Adjust preview window size (60% of window)
-            preview_width = 0.6,
-          },
-          vertical = {
-            -- Adjust preview height for vertical layout
-            preview_height = 0.5,
-          },
-          -- Move prompt to the top for better ergonomics
-          prompt_position = "top",
-        },
-        vimgrep_arguments = {
-          "rg",
-          "--color=never",
-          "--no-heading",
-          "--with-filename",
-          "--line-number",
-          "--column",
-          "--smart-case",
-        },
-        sorting_strategy = "ascending",
-        prompt_prefix = " 🔍 ",
-        selection_caret = " ",
-        color_devicons = true,
-
-        file_sorter = require("telescope.sorters").get_fuzzy_file,
-        file_ignore_patterns = { ".git/", "target/" },
-        path_display = { "smart" },
-
-        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-
-        -- Better mappings within Telescope prompts
-        mappings = {
-          i = {
-            ["<C-j>"] = require("telescope.actions").move_selection_next,
-            ["<C-k>"] = require("telescope.actions").move_selection_previous,
-            ["<C-q>"] = require("telescope.actions").send_to_qflist
-              + require("telescope.actions").open_qflist,
-          },
-        },
-      },
-      pickers = {
-        find_files = {
-          -- Show hidden files
-          hidden = false,
-          previewer = true,
-        },
-        live_grep = {
-          previewer = true,
-        },
-      },
-    },
-    config = function(_, opts)
-      local telescope = require("telescope")
-      local pickers = require("telescope.pickers")
-      local finders = require("telescope.finders")
-      local make_entry = require("telescope.make_entry")
+    opts = function()
+      local actions = require("telescope.actions")
+      local previewers = require("telescope.previewers")
+      local sorters = require("telescope.sorters")
       local conf = require("telescope.config").values
 
+      return {
+        defaults = {
+          layout_config = {
+            horizontal = {
+              preview_width = 0.6,
+            },
+            vertical = {
+              preview_height = 0.5,
+            },
+            prompt_position = "top",
+          },
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+          },
+          sorting_strategy = "ascending",
+          prompt_prefix = " 🔍 ",
+          selection_caret = " ",
+          color_devicons = true,
+
+          file_sorter = sorters.get_fuzzy_file,
+          file_ignore_patterns = { ".git/", "target/" },
+          path_display = { "smart" },
+
+          file_previewer = previewers.vim_buffer_cat.new,
+          grep_previewer = previewers.vim_buffer_vimgrep.new,
+          qflist_previewer = previewers.vim_buffer_qflist.new,
+
+          mappings = {
+            i = {
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+              -- Chaining actions:
+              ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+            },
+          },
+        },
+
+        pickers = {
+          find_files = {
+            hidden = false,
+            previewer = true,
+          },
+          live_grep = {
+            previewer = true,
+          },
+        },
+      }
+    end,
+    config = function(_, opts)
+      local telescope = require("telescope")
+      local finders = require("telescope.finders")
+      local pickers = require("telescope.pickers")
+      local make_entry = require("telescope.make_entry")
+      local sorters = require("telescope.sorters")
+      local conf = require("telescope.config").values
+
+      -- Custom multi-arg live grep
       local live_multigrep = function()
         local finder = finders.new_async_job({
           command_generator = function(prompt)
+            -- Exit early if prompt is empty or nil
             if not prompt or prompt == "" then
               return nil
             end
 
             local pieces = vim.split(prompt, "  ")
             local args = { "rg" }
+
             if pieces[1] then
               table.insert(args, "-e")
               table.insert(args, pieces[1])
@@ -89,7 +96,6 @@ return {
               table.insert(args, pieces[2])
             end
 
-            ---@diagnostic disable-next-line: deprecated
             return vim.tbl_flatten({
               args,
               {
@@ -112,40 +118,29 @@ return {
             prompt_title = "Multi Grep",
             finder = finder,
             previewer = conf.grep_previewer(opts),
-            sorter = require("telescope.sorters").empty(),
+            sorter = sorters.empty(),
           })
           :find()
       end
 
       telescope.setup(opts)
-      -- Load the FZF extension
       telescope.load_extension("fzf")
 
-      vim.keymap.set(
-        "n",
-        "<leader>gb",
-        require("telescope.builtin").git_branches
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>gc",
-        require("telescope.builtin").git_commits
-      )
-      vim.keymap.set("n", "<leader>gs", require("telescope.builtin").git_status)
-      vim.keymap.set(
-        "n",
-        "<leader>fb",
-        require("telescope.builtin").current_buffer_fuzzy_find
-      )
-      vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
-      vim.keymap.set("n", "<leader>fg", live_multigrep)
-      vim.keymap.set("n", "<leader>cb", require("telescope.builtin").buffers)
-      vim.keymap.set(
-        "n",
-        "<leader>gw",
-        require("telescope.builtin").grep_string
-      )
-      vim.keymap.set("n", "<leader>tj", require("telescope.builtin").help_tags)
+      -- Key mappings
+      local keymap = vim.keymap.set
+      local builtin = require("telescope.builtin")
+
+      keymap("n", "<leader>gb", builtin.git_branches)
+      keymap("n", "<leader>gc", builtin.git_commits)
+      keymap("n", "<leader>gs", builtin.git_status)
+
+      keymap("n", "<leader>fb", builtin.current_buffer_fuzzy_find)
+      keymap("n", "<leader>ff", builtin.find_files)
+      keymap("n", "<leader>fg", live_multigrep)
+      keymap("n", "<leader>cb", builtin.buffers)
+
+      keymap("n", "<leader>gw", builtin.grep_string)
+      keymap("n", "<leader>tj", builtin.help_tags)
     end,
   },
 }
