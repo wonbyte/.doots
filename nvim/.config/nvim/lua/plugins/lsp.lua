861afa6cce2cfe3946201384436e6f60db10de37
Greg Sieranski <git@wonbyte.com>
2024-12-29 23:52:19 -0500

    nvim: https://github.com/neovim/neovim/issues/30985

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -139,20 +139,6 @@ return {
         }, server_opts))
       end
 
-      -- Handle potential diagnostic-related issues
-      for _, method in ipairs({
-        "textDocument/diagnostic",
-        "workspace/diagnostic",
-      }) do
-        local default_handler = vim.lsp.handlers[method]
-        vim.lsp.handlers[method] = function(err, result, context, config)
-          if err ~= nil and err.code == -32802 then
-            return
-          end
-          return default_handler(err, result, context, config)
-        end
-      end
-
       -- LspAttach AutoCommand for Buffer-Local Keybindings
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
